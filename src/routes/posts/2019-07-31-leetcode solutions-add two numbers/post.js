07ce3084cf552eeb647b02a55afec8cfcc83dd53
guilong li <guilongli@ymail.com>
2019-08-03 16:41:06 -0500

    add reverse integer

--- src/routes/posts/2019-07-31-leetcode solutions-add two numbers/post.js
+++ src/routes/posts/2019-07-31-leetcode solutions-add two numbers/post.js
@@ -1,6 +1,6 @@
 export default {
-  title: `leetcode solutions-add two numbers`,
+  title: `leetcode solutions-add two numbers...`,
   tags: ['java', 'algorithm'],
-  spoiler: "add two numbers, ",
+  spoiler: "add two numbers, zigzag conversion, reverse integer",
   getContent: () => import('./document.mdx'),
 }
