f1b4be364c6c4c704ab9e2825202c0eba7002524
guilong li <guilongli@ymail.com>
2019-08-03 14:51:52 -0500

    add post 7/28

--- src/routes/posts/2019-07-28-how to update/post.js
+++ src/routes/posts/2019-07-28-how to update/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: `Leetcode solutions`,
+  title: `how to update`,
   tags: ['react', 'navi'],
   spoiler: "Learn how to add new posts and tweak the theme to your liking.",
   getContent: () => import('./document.mdx'),
---
+++ src/routes/posts/2019-07-29-formatwebpackmessages module not found error/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `formatwebpackmessages module not found error`,
+  tags: ['react', 'javascript'],
+  spoiler: "formatwebpackmessages module not found error.",
+  getContent: () => import('./document.mdx'),
+}
