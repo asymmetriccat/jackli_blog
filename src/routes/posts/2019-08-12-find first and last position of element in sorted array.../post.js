0ae534090bd16e06a46be5c873437c778112e8a1
guilong li <guilongli@ymail.com>
2019-08-12 16:56:47 -0500

    blog search and insert

--- src/routes/posts/2019-08-12-find first and last position of element in sorted array.../post.js
+++ src/routes/posts/2019-08-12-find first and last position of element in sorted array.../post.js
@@ -1,6 +1,6 @@
 export default {
   title: `LeetCode solutions-find first and last position of element in sorted array...`,
   tags: ['java', 'algorithm'],
-  spoiler: "find first and last position of element in sorted array",
+  spoiler: "find first and last position of element in sorted array, search-insert-position",
   getContent: () => import('./document.mdx'),
 }
