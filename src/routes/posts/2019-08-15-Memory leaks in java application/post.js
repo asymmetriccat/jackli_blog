7f2a7e450de3ef2ce48d56168994c45670155028
guilong li <guilongli@ymail.com>
2019-08-15 22:30:41 -0500

    blog-memory leaks

---
+++ src/routes/posts/2019-08-15-Memory leaks in java application/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Memory leaks in java application`,
+  tags: ['java', 'jvm', 'memory leak'],
+  spoiler: "java application memory leaks",
+  getContent: () => import('./document.mdx'),
+}
